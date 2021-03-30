0147263045811d10c8bf460685e9ff31fb0279b8
IKJOT123 <2020a1r128@mietjammu.in>
2021-03-30 22:08:58 +0530

    To_read_and_print_elements_in_two_dimensional_array

---
+++ 19_To_read_and_print_elements_in_two_dimensional_array.c
@@ -0,0 +1,27 @@
+#include<stdio.h> //Pre-Process directive to include standard input output header file
+int main(){ // main function body starting
+   //2D array declaration
+   int disp[2][3];
+
+   //Counter variables declaration for the loop
+   int i, j;
+
+   for(i=0; i<2; i++) { //Nested for loop
+      for(j=0;j<3;j++) {
+         printf("Enter value for disp[%d][%d]:", i, j); 
+         scanf("%d", &disp[i][j]); // taking input from the user
+
+      }
+   }
+   //Displaying array elements
+   printf("Two Dimensional array elements:\n");
+   for(i=0; i<2; i++) { // Nested for loop
+      for(j=0;j<3;j++) {
+         printf("%d ", disp[i][j]);
+         if(j==2){
+            printf("\n");
+         }
+      }
+   }
+   return 0;
+}
