94e666023c8698954bdc1438ca38736d5aaa4a38
IKJOT123 <2020a1r128@mietjammu.in>
2021-03-31 15:06:29 +0530

    array 10

--- 37_ArrayDeclaration.c
+++ 37_ArrayDeclaration.c
@@ -1,19 +1,19 @@
 #include <stdio.h> //Pre-Process directive to include standard input output header file.
 
 int main() { // main function body starting
-  int values[5]; // Array Declaration of size 5
+  int values[10]; // Array Declaration of size 5
 
-  printf("Enter 5 integers: "); //input integers
+  printf("Enter 10 integers: "); //input integers
 
   // taking input and storing it in an array
-  for(int i = 0; i < 5; ++i) {
+  for(int i = 0; i < 10; ++i) {
      scanf("%d", &values[i]);
   }
 
   printf("Displaying integers: ");
 
   // printing elements of an array
-  for(int i = 0; i < 5; ++i) {
+  for(int i = 0; i < 10; ++i) {
      printf("%d\n", values[i]);
   }
   return 0;
